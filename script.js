81850e9150798da040c3776b7cd36b3c66bf757b
Stephanie <stephykins@gmail.com>
2017-01-10 10:12:57 -0500

    add submit event to add notes to DOM

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 (function() {
 
   var writeNote = document.querySelector('#write-note');
+  var noteContainer = document.querySelector('.note-container');
   var colors = ['red-note', 'yellow-note', 'green-note', 'seafoam-note','lightblue-note', 'blue-note', 'purple-note', 'pink-note', 'darkpink-note'];
 
   function openWriteNote() {
     var writeInner = writeNote.innerHTML;
 
+    //only open the note if it's collapsed
     if(writeInner === 'Write note') {
       writeNote.innerHTML = '';
 
+      //create all the nodes for the writeNote div
       var input = document.createElement('input'),
           text = document.createElement('textarea'),
           colorSelect = document.createElement('div')
@@ -19,6 +22,7 @@
       text.setAttribute('placeholder', 'Write note');
       colorSelect.setAttribute('class', 'color-select');
 
+      //create the color selects
       for(var i = 0; i < 9; i++) {
         (function(i) {
           var color = document.createElement('div');
@@ -32,12 +36,41 @@
         }(i));
       }
 
+      //append new nodes to the dom
       submitButton.appendChild(buttonText);
       colorSelect.appendChild(submitButton);
       writeNote.appendChild(input);
       writeNote.appendChild(text);
       writeNote.appendChild(colorSelect);
+
+      //add note to the dom when user submits
+      submitButton.addEventListener('click', function(e) {
+        submitNote(input, text, e);
+      });
     }
+  }
+
+  function submitNote(input, text, e) {
+    var noteTitle = input.value,
+        noteText = text.value,
+        noteColor = writeNote.getAttribute('class');
+        note = document.createElement('div'),
+        title = document.createElement('h1'),
+        innerNoteText = document.createElement('p');
+
+    title.setAttribute('class', 'title');
+    title.innerText = noteTitle;
+    innerNoteText.innerText = noteText;
+    note.classList.add('note');
+    note.classList.add(noteColor);
+    note.appendChild(title);
+    note.appendChild(innerNoteText);
+    noteContainer.insertBefore(note, noteContainer.firstChild);
+
+    //reset write note div
+    writeNote.innerHTML = 'Write note';
+    writeNote.setAttribute('class', 'white-note');
+    e.stopPropagation();
   }
 
   writeNote.addEventListener('click', openWriteNote);
